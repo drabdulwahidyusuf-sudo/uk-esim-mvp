773b64fe6405ac2c180c8a53abb3f0099e7dce1f
Dr. Abdul Wahid Yusuf <drabdulwahidyusuf@gmail.com>
2025-11-12 20:09:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-uk-esim-mvp/
-  ├── server.js
-  └── package.json
+{
+  "name": "uk-esim-mvp",
+  "version": "1.0.0",
+  "main": "server.js",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "better-sqlite3": "^9.0.0",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.4.0",
+    "express": "^4.19.0"
+  }
+}
